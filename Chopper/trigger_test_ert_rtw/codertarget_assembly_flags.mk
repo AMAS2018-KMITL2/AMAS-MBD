a00bc535c88d3221e2c4afb7165a30a8a13bf05b
Vason Maitree <the.vincent@hotmail.com>
2018-03-18 19:09:43 +0700

    [FIX] Remove Useless File
    Remove Useless Configulation File

--- Chopper/trigger_test_ert_rtw/codertarget_assembly_flags.mk
+++
@@ -1,5 +0,0 @@
-ASFLAGS_ADDITIONAL = 
-TARGET_LOAD_CMD = 
-TARGET_LOAD_CMD_ARGS = 
-TARGET_PKG_INSTALLDIR = /Users/Vincent/Documents/MATLAB/SupportPackages/R2017a/toolbox/target/supportpackages/raspberrypi
-STACK_SIZE = 64
